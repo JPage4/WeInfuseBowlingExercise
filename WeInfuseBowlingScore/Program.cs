456f19b750187c7bd6c0a69002324a6908a93683
JPage4 <jesse.page@me.com>
2023-05-17 17:56:32 -0500

    removed quick test cases from Main()

--- WeInfuseBowlingScore/Program.cs
+++ WeInfuseBowlingScore/Program.cs
@@ -8,17 +8,9 @@ namespace WeInfuseBowlingScore
     {
         public static void Main(String[] args)
         {
-            string[] bowlingFrames = { "4", "5", "X", "8", "1", "3", "/", "5", "2", "4", "/", "X", "4", "/", "8", "1", "X", "X", "5" }; // extra frame on 10
-            //string[] bowlingFrames = { "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X" };
-            //string[] bowlingFrames = { "4", "5", "X", "8", "/"};
-            //string[] bowlingFrames = { "4", "5", "X", "8" };
-            //string[] bowlingFrames = { "4", "5", "X", "8", "1" };
-
-
-            //Console.WriteLine("Please enter your scores for each roll");
-            //var userInput = Console.ReadLine();
-            //string[] bowlingFrames = userInput.Split(" ");
-
+            Console.WriteLine("Please enter your scores for each roll");
+            var userInput = Console.ReadLine();
+            string[] bowlingFrames = userInput.Split(" ");
 
             BowlingScore score = new BowlingScore();
 
