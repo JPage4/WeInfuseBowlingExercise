205b486c9fe6ca7897287764d19eeffe8c69e772
JPage4 <jesse.page@me.com>
2023-05-17 18:31:08 -0500

    removed unnecessary using statements

--- WeInfuseBowlingScoreTests/BowlingScoreTests.cs
+++ WeInfuseBowlingScoreTests/BowlingScoreTests.cs
@@ -2,10 +2,6 @@
 using WeInfuseBowlingScore;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
 
 namespace WeInfuseBowlingScore.Tests
 {
